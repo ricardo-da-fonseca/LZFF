f1810faed27d4ad29fb6eb56f5b261fbaa9503c4
_ricardo_da_fonseca_ <ricardo.fonseca@unesp.br>
2024-09-20 17:22:59 -0300

    feat: implementação inicial da leitura de arquivos
    A função lê o caminho do arquivo e seleciona a extensão que determina qual a função de leitura do R deve ser chamada.

--- R/blupf90_F.R
+++ R/blupf90_F.R
@@ -1,7 +1,10 @@
 #' Função para formatação do arquivo de dados fenotípicos no software BLUPF90
 #'
-#' @param y1 caminho do arquivo de dados a ser formatado pela função
-#' @param y2 data frame com os dados a serem formatados pela função
+#' @param h valor lógico indicando a presença de cabeçalho
+#' @param s separador de campo utilizado no arquivo de dados
+#' @param d caractere utilizado como separador decimal
+#' @param md valor utilizado para indicar os valores perdidos no arquivo de dados
+#' @param local caminho do arquivo
 #'
 #' @return um arquivo de dados no formato adequado para rodar o software
 #' @export
@@ -10,12 +13,21 @@
 #' y<-data.frame(id=1:5,dados=rnorm(5,10,3))
 #' blupf90_F(y)
 
-blupf90_F<-function(y1=NULL,y2=NULL){
-  if(is.null(y1)){
-    print("Codificar a parte do arquivo aqui.")
-    print(y1)
+blupf90_F<-function(local,h=TRUE,s=" ",d=",",md=""){
+  tipo<-stringr::str_extract(local,"(\\w+)$")
+  print(tipo)
+  if(tipo=="csv"){
+    dados<-read.csv(local,header=h,sep=s,dec=d,na.strings = md)
   } else{
-    print("Codificar a rotina o data.frame aqui.")
-    print(y2)
+    if(tipo=="xls" || tipo=="xlsx"){
+      dados<-readxl::read_excel(local,na = md)
+    } else{
+      if(tipo=="txt"){
+        dados<-read.table(local,header=h,sep=s,dec=d,na.strings = md)
+      } else{
+        stop("Não detectei a extensão do arquivo. Por favor, forneça um arquivo com nome e extensão.")
+      }
+    }
   }
+  dados
 }
