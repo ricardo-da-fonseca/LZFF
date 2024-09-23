4be2c4bac0e204b39103b65b387e2abc0b3791ea
_ricardo_da_fonseca_ <ricardo.fonseca@unesp.br>
2024-09-23 17:48:09 -0300

    feat(blupf90_F): Implementação da leitura do arquivo pela função

--- R/blupf90_F.R
+++ R/blupf90_F.R
@@ -1,33 +1,41 @@
-#' Função para formatação do arquivo de dados fenotípicos no software BLUPF90
+#' Function for phenotypic data file formatting to BLUPF90 software
 #'
-#' @param h valor lógico indicando a presença de cabeçalho
-#' @param s separador de campo utilizado no arquivo de dados
-#' @param d caractere utilizado como separador decimal
-#' @param md valor utilizado para indicar os valores perdidos no arquivo de dados
-#' @param local caminho do arquivo
+#' @param local path of the data file
+#' @param h logical value indicating the presence of header
+#' @param s field separator used in data file
+#' @param d character used as decimal separator
+#' @param md value used to indicate missing values in data file
+#' @param omd missing data value to be written in the output file
 #'
-#' @return um arquivo de dados no formato adequado para rodar o software
+#' @return a data file in the appropriate format to run the software
 #' @export
 #'
 #' @examples
-#' y<-data.frame(id=1:5,dados=rnorm(5,10,3))
-#' blupf90_F(y)
+#'
 
-blupf90_F<-function(local,h=TRUE,s=" ",d=",",md=""){
+blupf90_F<-function(local,h=TRUE,s=" ",d=",",md="",of="blupF90_data.csv",omd=0){
   tipo<-stringr::str_extract(local,"(\\w+)$")
   print(tipo)
   if(tipo=="csv"){
-    dados<-read.csv(local,header=h,sep=s,dec=d,na.strings = md)
+    dados<-utils::read.csv(local,header=h,sep=s,dec=d,na.strings = md)
   } else{
     if(tipo=="xls" || tipo=="xlsx"){
       dados<-readxl::read_excel(local,na = md)
     } else{
       if(tipo=="txt"){
-        dados<-read.table(local,header=h,sep=s,dec=d,na.strings = md)
+        dados<-utils::read.table(local,header=h,sep=s,dec=d,na.strings = md)
       } else{
-        stop("Não detectei a extensão do arquivo. Por favor, forneça um arquivo com nome e extensão.")
+        stop("No file extension. Please, provide a file name with extension.")
       }
     }
   }
-  dados
+  #checking if all values are numeric
+  isnum<-sapply(dados)
+  for(i in 1:length(isnum)){
+    if(!isnum[i]){
+      print("The data file has non-numerical values, which must be recoded.\n
+            I will do a simple recode for you but the renumF90 program should be used anyway.")
+      simpleRecode
+    }
+      }
 }
