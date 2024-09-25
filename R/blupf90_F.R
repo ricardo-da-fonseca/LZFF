95495707b4501ff2b12a857fc2916d3d2bca9089
_ricardo_da_fonseca_ <ricardo.fonseca@unesp.br>
2024-09-25 11:25:57 -0300

    feat: Finalizando a função com mais algumas implementações
    [Novas Implementações]
    1. checagem de valores alfanuméricos
    2. checagem da presença de # no nome do arquivo de saída
    3. recodificação das colunas alfanuméricas
    4. Substituição dos NAS pelos valores de dados perdidos passados pelo usuário
    5. Escrita do arquivo de dados para o BLUF90.

--- R/blupf90_F.R
+++ R/blupf90_F.R
@@ -5,15 +5,17 @@
 #' @param s field separator used in data file
 #' @param d character used as decimal separator
 #' @param md value used to indicate missing values in data file
+#' @param of name of the output file
 #' @param omd missing data value to be written in the output file
 #'
 #' @return a data file in the appropriate format to run the software
 #' @export
 #'
-#' @examples
-#'
 
-blupf90_F<-function(local,h=TRUE,s=" ",d=",",md="",of="blupF90_data.csv",omd=0){
+blupf90_F<-function(local,h=TRUE,s=" ",d=",",md="",of="blupF90_data",omd=0){
+  ok_of<-if(stringr::str_extract(of,"#")){
+    stop("File name cannot contain a #. Choose a name without a #")
+  }
   tipo<-stringr::str_extract(local,"(\\w+)$")
   print(tipo)
   if(tipo=="csv"){
@@ -25,17 +27,29 @@ blupf90_F<-function(local,h=TRUE,s=" ",d=",",md="",of="blupF90_data.csv",omd=0){
       if(tipo=="txt"){
         dados<-utils::read.table(local,header=h,sep=s,dec=d,na.strings = md)
       } else{
-        stop("No file extension. Please, provide a file name with extension.")
+        stop("Extension unknown or absent. Please, provide a file name with an supported extension (csv, xls, xlsx or txt.")
       }
     }
   }
   #checking if all values are numeric
-  isnum<-sapply(dados)
+  isnum<-sapply(dados,is.numeric)
+  mapList<-list()
   for(i in 1:length(isnum)){
     if(!isnum[i]){
       print("The data file has non-numerical values, which must be recoded.\n
             I will do a simple recode for you but the renumF90 program should be used anyway.")
-      simpleRecode
+      maplist<-append(maplist,simpleRecode(dados[i]))
+      dados[i]<-maplist[[i]]$Recodes
     }
-      }
+  }
+  #Replacing NAs
+  vnas<-sapply(dados,is.na)
+  for(i in 1:length(vnas)){
+    if(vnas[i]==TRUE){
+      is.na(dados[i])<-omd
+    }
+  }
+  # Writing the formated data file
+  of<-paste(of,".txt",sep="")
+  utils::write.table(dados,of,quote=FALSE,sep=" ")
 }
